f39e815993a39f13d712b69e48f692a8562eb61c
Sonia Justiniano <sjustinianoayte@gmail.com>
2023-02-22 14:37:27 -0500

    mejorando el código

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:codigo_quiz/question.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,15 +9,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int questionNumber = 0;
 
-  List<String> question = [
-    "¿He dormido 8 horas?",
-    "¿La tierra es plana?",
-    "¿El hombre fue a la luna?",
-    "¿Ella me quieres?",
-    "¿He almorzado hoy días?",
+  List<Question> question = [
+    Question(questiontext: "¿He dormido 8 horas?", questionAnswer: false),
+    Question(questiontext: "¿La tierra es plana?", questionAnswer: false),
+    Question(questiontext: "¿El hombre fue a la luna?", questionAnswer: true),
+    Question(questiontext: "¿Ella me quieres?", questionAnswer: false),
+    Question(questiontext: "¿He almorzado hoy días?", questionAnswer: true),
   ];
 
-  List<bool> answers = [false, false, true, false, true];
+  // List<String> question = [
+  //   "¿He dormido 8 horas?",
+  //   "¿La tierra es plana?",
+  //   "¿El hombre fue a la luna?",
+  //   "¿Ella me quieres?",
+  //   "¿He almorzado hoy días?",
+  // ];
+
+  // List<bool> answers = [
+  //   false,
+  //   false,
+  //   true,
+  //   false,
+  //   true,
+  // ];
 
   List<Icon> scoreKeeper = [];
 
@@ -33,7 +48,7 @@ class _HomePageState extends State<HomePage> {
               child: Padding(
                 padding: const EdgeInsets.all(6.0),
                 child: Text(
-                  question[questionNumber],
+                  question[questionNumber].questiontext,
                   textAlign: TextAlign.center,
                   style: const TextStyle(
                     fontSize: 22.0,
@@ -58,7 +73,7 @@ class _HomePageState extends State<HomePage> {
               child: MaterialButton(
                 onPressed: () {
                   //Obtenemos la respuesta correcta de  la pregunta
-                  bool correctAnswer = answers[questionNumber];
+                  bool correctAnswer = question[questionNumber].questionAnswer;
 
                   //Comparamos la respuesta con el valor verdadero
                   if (correctAnswer == true) {
@@ -101,23 +116,25 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.all(8.0),
               child: MaterialButton(
                 onPressed: () {
-                  // bool correctAnswer = answers[questionNumber];
-                  // if (correctAnswer == false) {
-                  // print("La respuesta es correcta: $correctAnswer");
-                  //   scoreKeeper.add(
-                  //     const Icon(
-                  //       Icons.check,
-                  //       color: Colors.green,
-                  //     ),
-                  //   );
-                  // } else {
-                  //   scoreKeeper.add(
-                  //     const Icon(
-                  //       Icons.close,
-                  //       color: Colors.red,
-                  //    ),
-                  // );
-                  //  }
+                  bool correctAnswer = question[questionNumber].questionAnswer;
+                  if (correctAnswer == false) {
+                    //print("La respuesta es correcta: $correctAnswer");
+                    scoreKeeper.add(
+                      const Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                  } else {
+                    scoreKeeper.add(
+                      const Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                  }
+                  setState(() {});
+                  questionNumber++;
                 },
                 color: const Color(0xffff6b6b),
                 child: const Text(
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questiontext;
+  bool questionAnswer;
+
+  Question({
+    required this.questiontext,
+    required this.questionAnswer,
+  });
+}
